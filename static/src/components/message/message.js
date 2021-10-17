cb8f13a5b8a086a6159e843bac399d15bbadbf4c
Philippe L'ATTENTION <philippe.lattention@hotmail.fr>
2021-10-17 10:34:56 +0400

    [IMP] Adding the patchInstanceMethods example

--- static/src/components/message/message.js
+++ static/src/components/message/message.js
@@ -114,33 +114,42 @@ odoo.define(
     MessageList.components.Message = PatchableMessage;
 
     // Solution 4
+    // Using the patchInstanceMethods
     const {
       patchInstanceMethods,
     } = require("owl_tutorial_extend_override_components.utils");
-    patchInstanceMethods(
-      components.Message.prototype,
-      "owl_tutorial_extend_override_components/static/src/components/message/message.js",
-      {
-        /**
-         * Get the avatar of the user. This function can be overriden
-         *
-         * @returns {string}
-         */
-        get avatar() {
-          if (
-            this.message.author &&
-            this.message.author === this.env.messaging.partnerRoot
-          ) {
-            // Here we replace the Robot with the better CodingDodo Avatar
-            return "https://avatars.githubusercontent.com/u/81346769?s=400&u=614004f5f4dace9b3cf743ee6aa3069bff6659a2&v=4";
-          } else if (this.message.author) {
-            return this.message.author.avatarUrl;
-          } else if (this.message.message_type === "email") {
-            return "/mail/static/src/img/email_icon.png";
-          }
-          return "/mail/static/src/img/smiley/avatar.jpg";
-        },
-      }
-    );
+    patchInstanceMethods(components.Message.prototype, "messageFirstPatch", {
+      /**
+       * Get the avatar of the user. This function can be overriden
+       *
+       * @returns {string}
+       */
+      get avatar() {
+        if (
+          this.message.author &&
+          this.message.author === this.env.messaging.partnerRoot
+        ) {
+          // Here we replace the Robot with the better CodingDodo Avatar
+          return "https://avatars.githubusercontent.com/u/81346769?s=400&u=614004f5f4dace9b3cf743ee6aa3069bff6659a2&v=4";
+        } else if (this.message.author) {
+          return this.message.author.avatarUrl;
+        } else if (this.message.message_type === "email") {
+          return "/mail/static/src/img/email_icon.png";
+        }
+        return "/mail/static/src/img/smiley/avatar.jpg";
+      },
+    });
+
+    patchInstanceMethods(components.Message.prototype, "messageSecondPatch", {
+      /**
+       * Override of override
+       *
+       * @returns {string}
+       */
+      get avatar() {
+        let originAvatar = this._super(...arguments);
+        return originAvatar + "?overridenPatch=Yes";
+      },
+    });
   }
 );
